cc601d3be5a7da29867d9bb93557507791ea9741
uytvy <uytvy@student.kit.edu>
2022-12-04 02:10:49 +0100

    -U: size modification through buttons -U: size display and updates at every button press

---
+++ index.js
@@ -0,0 +1,14 @@
+function testFunction() {
+  var el = document.getElementById("title");
+  var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+  var fontSize = parseFloat(style);
+  el.style.fontSize = (fontSize + 1) + 'px';
+  document.getElementById("demo").innerHTML = fontSize;
+}
+function secondtestFunction() {
+  var el = document.getElementById("title");
+  var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+  var fontSize = parseFloat(style);
+  el.style.fontSize = (fontSize - 1) + 'px';
+  document.getElementById("demo").innerHTML = fontSize;
+}
